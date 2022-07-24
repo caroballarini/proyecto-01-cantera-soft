2865667fc35cde57d877fe400f917cbf757fffb9
Caro Ballarini <carolinaballarini@live.com.ar>
2022-07-24 19:59:31 -0300

    realicé el primer algoritmo

---
+++ algoritmos.js
@@ -0,0 +1,18 @@
+var a;
+var b;
+var c;
+
+function suma (a, b)
+{
+    return c=a + b;
+}
+
+console.log (suma(10,5));
+
+if (c<5)
+{
+    console.log ("el número es menor a 5")
+} 
+else {
+    console.log ("el número es mayor a 5")
+}
